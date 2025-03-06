9632a2e1e49aadff32646f7b4be39d39efeaabfc
HBY <hbigpillar@gmail.com>
2025-03-06 22:07:31 +0900

    first commit

---
+++ src/main/java/com/artu/fullstack_team_project_administrator/FullstackTeamProjectAdministratorApplication.java
@@ -0,0 +1,13 @@
+package com.artu.fullstack_team_project_administrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackTeamProjectAdministratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackTeamProjectAdministratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artu/fullstack_team_project_administrator/FullstackTeamProjectAdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artu.fullstack_team_project_administrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackTeamProjectAdministratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
